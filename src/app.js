3734184bf2ed605b8ad5e36487b20318ba879fe4
Tushar Pokle <tushar.pokle@gmail.com>
2016-05-19 00:48:39 +1000

    reduced js

--- src/app.js
+++ src/app.js
@@ -1,14 +1,13 @@
 import React from "react"
+import ReactDOM from "react-dom"
+
 import Counter from "./counter"
-import {reddish} from "./styles"
 
-const {Component} = React
-
-export default class App extends Component {
+export default class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>hejjjj</h1>
+        <h1>hi</h1>
 
         <Counter
           initialValue={0}
@@ -17,9 +16,15 @@ export default class App extends Component {
         <Counter
           initialValue={0}
           step={1}
-          interval={100} />
+          interval={1000} />
+        <Counter
+          initialValue={0}
+          step={1}
+          interval={500} />
       </div>
     )
   }
 }
 
+
+ReactDOM.render(<p><App/></p>, document.getElementById("app"))
